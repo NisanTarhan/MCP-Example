48ea220dfe74123e38eb6f272d7b46f65ef9246d
Nisan Tarhan <nisantarhan@Nisan-MacBook-Air.local>
2025-07-20 21:51:08 +0300

    Tool created to add a user

---
+++ src/server.ts
@@ -0,0 +1,71 @@
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import fs from "node:fs/promises";
+import { CreateMessageResultSchema } from "@modelcontextprotocol/sdk/types.js";
+
+type UserType = {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+};
+
+const server = new McpServer({
+  name: "test-mcp-server",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+    prompts: {},
+  },
+});
+
+server.registerTool(
+  "create-user",
+  {
+    title: "Create User",
+    description: "Creates a new user with the provided details.",
+    inputSchema: {
+      name: z.string(),
+      email: z.string(),
+      address: z.string(),
+      phone: z.string(),
+    },
+  },
+  async (params) => {
+    try {
+      const id = await createUser(params);
+      return {
+        content: [{ type: "text", text: `User ${id} created successfully!` }],
+      };
+    } catch {
+      return {
+        content: [{ type: "text", text: "Failed to save user!" }],
+      };
+    }
+  }
+);
+
+async function createUser(user: UserType) {
+  //experimental: JSON import which allows us to import user data to JSON file
+  const users = await import("./data/users.json", {
+    with: { type: "json" },
+  }).then((mod) => mod.default);
+
+  const id = users.length + 1;
+  users.push({ id, ...user });
+
+  await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2));
+  return id;
+}
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main();
